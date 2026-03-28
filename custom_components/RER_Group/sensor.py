878b7ea61677e46969dab80f744c3a9b0fbfd93a
v1n3ri <49840915+v1n3ri@users.noreply.github.com>
2026-03-28 17:50:30 +0200

    Update sensor.py with Home Assistant sensor implementation

--- custom_components/RER_Group/sensor.py
+++ custom_components/RER_Group/sensor.py
@@ -1,22 +1,48 @@
-from homeassistant.helpers.entity import DeviceInfo  
-from homeassistant.components.sensor import SensorEntity  
+import logging
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.entity import DeviceInfo
 
-class RERGroupSensor(SensorEntity):  
-    def __init__(self, name, device_email):  
-        self._name = name  
-        self._unique_id = name.lower().replace(' ', '_')  
-        self._device_email = device_email  
-        self._device_info = DeviceInfo(  
-            identifiers={("rer_group", self._unique_id)},  
-            name=self._name,  
-            manufacturer="RER Group",  
-            model="Group Device",  
-            sw_version="1.0",  
-            configuration_url=f"https://example.com/devices/{self._unique_id}",  
-        )  
+DOMAIN = "rer_group"
 
-    @property  
-    def device_info(self):  
-        return self._device_info  
+_LOGGER = logging.getLogger(__name__)
 
-    # Additional implementation details for your sensor would go here...  
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up sensors from a config entry."""
+    sensors = []  # Initialize list for sensor entities
+    # Create sensor instances here and append to sensors list
+    async_add_entities(sensors, update_before_add=True)
+
+class RERGroupSensor(SensorEntity):
+    """Representation of a RER Group sensor."""
+
+    def __init__(self, name, unique_id, device_info):
+        self._name = name
+        self._unique_id = unique_id
+        self._device_info = device_info
+        self._state = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def unique_id(self):
+        return self._unique_id
+
+    @property
+    def device_info(self):
+        return self._device_info
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
+        self.async_write_ha_state()
+
+# Function to setup device grouping
+async def async_setup_device_group(hass, config_entry):
+    # Implement device grouping logic here
+    pass
